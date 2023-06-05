ef9e83f0a43226cfdc761a922b1711ee68607612
MarkNillesen <126663116+MarkNillesen@users.noreply.github.com>
2023-06-05 12:28:25 +0200

    Add files via upload

---
+++ InsertCoin-web-Mark/Insert-Coin/FAQ.js
@@ -0,0 +1,119 @@
+i = 1;
+a = 1;
+b = 1;
+c = 1;
+d = 1;
+e = 1;
+f = 1;
+g = 1;
+h = 1;
+i = 1;
+j = 1;
+
+function myFunction1() {
+          
+    if(i == 1){
+       document.getElementById("myDIV1").innerHTML = "1. Minecraft is een sandbox-game waarbij spelers kunnen bouwen en avondturen beleven in een virtueel wereld.";
+        i = i = 0
+    }
+    else{
+        document.getElementById("myDIV1").innerHTML = "";
+        i = i = 1
+    }
+}
+
+function myFunction2() {
+    if(a == 1){
+        document.getElementById("myDIV2").innerHTML = "2. Het begint in 10 februari om 13:00.";
+        a = a = 0
+    }
+    else{
+        document.getElementById("myDIV2").innerHTML = "";
+        a = a = 1
+    }
+}
+
+function myFunction3() {
+    if(b == 1){
+        document.getElementById("myDIV3").innerHTML = "3. Het is van 09:00 uur tot 18:00 uur.";
+        b = b = 0
+    }
+    else{
+        document.getElementById("myDIV3").innerHTML = "";
+        b = b = 1
+    }
+}
+
+function myFunction4() {
+    if(c == 1){
+        document.getElementById("myDIV4").innerHTML = "4. Insert coin organiseert in Nederland en België.";
+        c = c = 0
+    }
+    else{
+        document.getElementById("myDIV4").innerHTML = "";
+        c = c = 1
+        }
+}
+        
+function myFunction5() {
+          if(d == 1){
+            document.getElementById("myDIV5").innerHTML = "5. Nee, inster coin organiseert alleen een jaarlijkse toernooi";
+            d = d = 0
+          }
+          else{
+            document.getElementById("myDIV5").innerHTML = "";
+            d = d = 1
+          }
+}
+
+function myFunction6() {
+          if(e == 1){
+            document.getElementById("myDIV6").innerHTML = "6. Er kunnen 20 teams van 5 mensen mee doen.";
+            e = e = 0
+          }
+          else{
+            document.getElementById("myDIV6").innerHTML = "";
+            e = e = 1
+          }
+        }
+        function myFunction7() {
+          if(f == 1){
+            document.getElementById("myDIV7").innerHTML = "7. Ja, je kunt €250 winnen als je 1ste plaats krijgt.";
+            f = f = 0
+          }
+          else{
+            document.getElementById("myDIV7").innerHTML = "";
+            f = f = 1
+          }
+        }
+
+        function myFunction8() {
+          if(g == 1){
+            document.getElementById("myDIV8").innerHTML = "8. Je moet minimaal 15 jaar oud zijn om mee te doen.";
+            g = g = 0
+          }
+          else{
+            document.getElementById("myDIV8").innerHTML = "";
+            g = g = 1
+          }
+        }
+        function myFunction9() {
+          if(i == 1){
+            document.getElementById("myDIV9").innerHTML = "9. Ja, tijdens de toernooi kan je eten kopen bij ons.";
+            i = i = 0
+          }
+          else{
+            document.getElementById("myDIV9").innerHTML = "";
+            i = i = 1
+          }
+        }
+        function myFunction10() {
+          if(j == 1){
+            document.getElementById("myDIV10").innerHTML = "10. Nee, Wij zorgen voor de spullen";
+            j = j = 0
+          }
+          else{
+            document.getElementById("myDIV10").innerHTML = "";
+            j = j = 1
+          }
+        }
